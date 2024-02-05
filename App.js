44021433759ec07cbab5a58b463048fec732fb3d
Muhammad Bilal <mbilal254.m@gmail.com>
2024-02-05 16:55:46 +0500

    Create custom Component
    Create custom Component for day 1 and passing data with props

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import DayListItem from "./src/Components/core/DayListItem";
+
+const days = [...Array(24)].map((e, i) => i + 1);
 
 export default function App() {
+  console.log(days);
   return (
     <View style={styles.container}>
-      <Text>Hello World! </Text>
+      <FlatList
+        data={days}
+        numColumns={2}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        renderItem={({ item }) => <DayListItem day={item} />}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +25,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: { gap: 10 },
 });
---
+++ src/Components/core/DayListItem.jsx
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function DayListItem({ day }) {
+  return (
+    <View style={styles.box}>
+      <Text style={styles.text}>{day}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "#F9EDE3",
+    flex: 1,
+    aspectRatio: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#9b4521",
+  },
+  text: {
+    fontSize: 72,
+    color: "#9b4521",
+  },
+});
+
+export default DayListItem;
